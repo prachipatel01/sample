fe0563cd3077b4fae3345f3a227d61dfec3a5554
prachipatel01 <57019866+prachipatel01@users.noreply.github.com>
2020-12-08 20:10:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Place Your Startup</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		    <link rel="stylesheet" href="assets/css/index.css" />
+		    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+	</head>
+	<body class="homepage">
+    <h1>Hello world</h1>
+  </body>
+  </html>
